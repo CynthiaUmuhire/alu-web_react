77c14d0bd1abfa07243cf442bc344b20fb1750fe
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-01-07 07:01:30 +0200

    Basic application

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+export default function App (){
+    return (
+        <>
+        <header class="App-header">
+            <img src='../holbertonLogo.jpg' alt='holberton logo' />
+            <h1 class="App-heading">School dashboard</h1>
+        </header>
+        <div class ='Divider'></div>
+        <body class="App-body">
+            <p>Login to access the full dashboard</p>
+        </body>
+        <footer class="App-footer">
+        <div class ='Divider'></div>
+            <p>Copyright 2020 - holberton School</p>
+        </footer>
+        </>
+    )
+    }
